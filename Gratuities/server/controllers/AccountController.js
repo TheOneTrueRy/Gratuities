cc10329c5cfdde76552e14431305492d875a8f30
Andrew Meyers <laxmeyers@gmail.com>
2023-03-08 15:04:36 -0700

    edit account

--- Gratuities/server/controllers/AccountController.js
+++ Gratuities/server/controllers/AccountController.js
@@ -12,7 +12,7 @@ export class AccountController extends BaseController {
   }
   async editAccount(req, res, next) {
     try {
-      const account = await accountService.updateAccount(req.userinfo, req.body)
+      const account = await accountService.updateAccount(req.userInfo, req.body)
       res.send(account)
     } catch (error) {
       next(error)
