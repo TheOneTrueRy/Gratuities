a29676c62670eea9535a0b9e1d767714a2c94e2b
JoeCalvi <joecalvi@me.com>
2023-03-08 14:47:36 -0700

    added todo

--- Gratuities.client/src/services/BusinessesService.js
+++ Gratuities.client/src/services/BusinessesService.js
@@ -8,6 +8,7 @@ class BusinessesService {
     async getHighestRatedBusinesses() {
         const res = await api.get('api/businesses')
         AppState.businesses = res.data.map(b => new Business(b))
+        // TODO need to add rating to businesses once we get profile ratings
         logger.log('[APPSTATE.businesses]', AppState.businesses)
     }
 }
