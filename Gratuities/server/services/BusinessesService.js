ceae21d645bc63558954a24bfd7e31c3dd658f53
Bryson Bloom <brysonbloom@icloud.com>
2023-03-08 12:12:50 -0700

    get business by id

--- Gratuities/server/services/BusinessesService.js
+++ Gratuities/server/services/BusinessesService.js
@@ -1,6 +1,10 @@
 import { dbContext } from "../db/DbContext";
 
 class BusinessesService {
+    async getBusinessById(businessId) {
+        const business = await dbContext.Businesses.findById(businessId).populate('owner', 'name picture email');
+        return business
+    }
     async createBusiness(body) {
         const business = await (await dbContext.Businesses.create(body))
         await business.populate('owner', 'name picture email')
