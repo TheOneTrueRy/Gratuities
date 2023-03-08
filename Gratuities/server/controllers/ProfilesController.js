2cc6fc07789081f173738f06227b7195d2e099b1
Andrew Meyers <laxmeyers@gmail.com>
2023-03-08 16:15:43 -0700

    tips make

--- Gratuities/server/controllers/ProfilesController.js
+++ Gratuities/server/controllers/ProfilesController.js
@@ -1,4 +1,6 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
 import { profileService } from '../services/ProfileService.js'
+import { tipsService } from '../services/TipsService.js'
 import BaseController from '../utils/BaseController'
 
 export class ProfilesController extends BaseController {
@@ -7,6 +9,20 @@ export class ProfilesController extends BaseController {
     this.router
       .get('', this.getProfiles)
       .get('/:id', this.getProfile)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+    .post('/:profileId/tips', this.giveTip)
+  }
+  async giveTip(req,res,next) {
+    try {
+      const recieverId = req.params.profileId
+      const tip = req.body
+      tip.giverId = req.userInfo.id
+      tip.recieverId = recieverId
+      const tips = await tipsService.giveTip(tip)
+      return res.send(tips)
+    } catch (error) {
+      next(error)
+    }
   }
 
   async getProfiles(req, res, next) {
--- Gratuities/server/services/TipsService.js
+++ Gratuities/server/services/TipsService.js
@@ -1,6 +1,16 @@
 import { dbContext } from "../db/DbContext"
+import { BadRequest } from "../utils/Errors"
+import {profileService} from "./ProfileService"
 
 class TipsService {
+    async giveTip(tip) {
+        if (!profileService.getProfileById(tip.recieverId)) {
+          throw new BadRequest('This user does not exist')
+        }
+        const tips = await dbContext.Tips.create(tip)
+        await tips.populate('giver', 'name picture')
+        return tips
+    }
     async getMyTips(userId) {
         const myTips = await dbContext.Tips.find({ recieverId: userId })
         return myTips
