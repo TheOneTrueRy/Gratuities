b1e2101df93d28446854dafc4c7a4da2eb71f732
Andrew Meyers <laxmeyers@gmail.com>
2023-03-08 16:19:16 -0700

    tipt has tip

--- Gratuities/server/models/Tip.js
+++ Gratuities/server/models/Tip.js
@@ -3,7 +3,8 @@ import { Schema } from "mongoose";
 
 export const TipSchema = new Schema({
     giverId: {type: Schema.Types.ObjectId, required: true, ref: "Account"},
-    recieverId: {type: Schema.Types.ObjectId, required: true, ref: "Account"}
+    recieverId: { type: Schema.Types.ObjectId, required: true, ref: "Account" },
+    tip: {type: Number, required:true, min:0.01}
 }, { timestamps: true, toJSON: { virtuals: true } })
 
 TipSchema.virtual('giver', {
