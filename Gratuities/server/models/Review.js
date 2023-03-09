073378eabef38ecef8d465af57922c8f8f74b4a4
Andrew Meyers <laxmeyers@gmail.com>
2023-03-09 11:21:04 -0700

    capitals

--- Gratuities/server/models/Review.js
+++ Gratuities/server/models/Review.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 export const ReviewSchema = new Schema({
-    creatorId: { type: Schema.Types.ObjectId, ref: 'account', required: true },
-    reviewedId: { type: Schema.Types.ObjectId, ref: 'account', required: true },
+    creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
+    reviewedId: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
     body: { type: String, maxLength: 500 },
     rating: { type: Number, enum: [0, 1, 2, 3, 4, 5], required: true },
 })
