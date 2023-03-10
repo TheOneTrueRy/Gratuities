1b66c25bbaf2eb9e1b3fe25203ef77bf2a07c228
JoeCalvi <joecalvi@me.com>
2023-03-10 12:02:48 -0700

    added scroll to profile card bio

--- Gratuities.client/src/components/ProfileCard.vue
+++ Gratuities.client/src/components/ProfileCard.vue
@@ -2,11 +2,14 @@
     <div class="ProfileCard">
         <router-link :to="{ name: 'Profile', params: { profileId: profile.id } }" class="text-light">
             <div class="row">
-                <div class="col-4 d-flex align-items-center">
+                <div class="col-3 d-flex align-items-center">
                     <img class="profile-picture-small selectable" :src="profile.picture" :alt="profile.picture">
                 </div>
-                <div class="col-8">
-                    <p>{{ profile.name }}<br>*****<br>{{ profile.bio }}</p>
+                <div class="col-9">
+                    <h6 class="text-center">{{ profile.name }} | {{ profile.rating }} Stars</h6>
+                    <div class="scroller">
+                        <p>"{{ profile.bio }}"</p>
+                    </div>
                 </div>
             </div>
         </router-link>
@@ -37,5 +40,12 @@ export default {
     width: 10vh;
     border-radius: 50%;
     border: 2px solid black;
+}
+
+.scroller {
+    height: 40px;
+    overflow-y: scroll;
+    scrollbar-color: #EF476F;
+    scrollbar-width: thin;
 }
 </style>
