247c3c8d5864d54a231f31db95143c142491d71b
Andrew Meyers <laxmeyers@gmail.com>
2023-03-10 11:49:34 -0700

    reviews ccourier

--- Gratuities/server/services/ReviewsService.js
+++ Gratuities/server/services/ReviewsService.js
@@ -1,3 +1,4 @@
+import { courier } from "../../authkey"
 import { dbContext } from "../db/DbContext"
 import { profileService } from "./ProfileService"
 
@@ -7,11 +8,26 @@ class ReviewsService {
         return reviews
     }
     async giveReview(body) {
-        const giver = await profileService.getProfileById(body.creatorId)
+        const reviewer = await profileService.getProfileById(body.creatorId)
         const receiver = await profileService.getProfileById(body.reviewedId)
-        
+
         const review = await dbContext.Reviews.create(body)
         await review.populate('creator', 'name picture')
+
+        await courier.send({
+            message: {
+                to: {
+                    email: receiver.email,
+                },
+                template: "4K1YX40ZRA4F9FQ7R11DX6575RAP",
+                data: {
+                    firstName: receiver.name,
+                    senderName: reviewer.name,
+                    reviewRating: body.rating,
+                    reviewBody: body.body
+                },
+            },
+        });
         return review
     }
 
--- Gratuities/server/services/TipsService.js
+++ Gratuities/server/services/TipsService.js
@@ -26,7 +26,9 @@ class TipsService {
 
     await courier.send({
       message: {
-        to: {email: receiver.email},
+        to: {
+          email: receiver.email
+        },
         template: "Y8REJMFD13M9MFQJVRRXH45T3HHA",
         data: {
           receiverName: receiver.name,
