05912cee024f4cad8a5316606a0445361d8a57d8
Jonah <jonah.osito@icloud.com>
2023-03-13 15:12:25 -0600

    !

--- Gratuities.client/src/pages/ProfilePage.vue
+++ Gratuities.client/src/pages/ProfilePage.vue
@@ -10,7 +10,7 @@
                                 <ProfileStarRating :rating="profile?.rating" />
                             </div>
                         </div>
-                        <div class="col-6 offset-md-6 text-end" v-if="account?.id == !profile?.id">
+                        <div class="col-6 offset-md-6 text-end" v-if="account?.id != profile?.id">
                             <div class="btn dropstart">
                                 <button class="btn btn-secondary-outline btn-sm dropdown-toggle fs-4" type="button"
                                     data-bs-toggle="dropdown" aria-expanded="false">
