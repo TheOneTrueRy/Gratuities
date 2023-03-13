d7f38107636e71369d5dc0767757b1cbc9d64cce
Ryan Wittman <OfficiallyRy@outlook.com>
2023-03-13 11:48:09 -0600

    fixed stars on Account page now teehee

--- Gratuities.client/src/components/MyStarRating.vue
+++ Gratuities.client/src/components/MyStarRating.vue
@@ -1,35 +1,38 @@
 <template>
-        <h3 v-if="account?.rating == 0.5"><i class="mdi mdi-star-half star"></i><i class="mdi mdi-star-outline"></i><i
-                        class="mdi mdi-star-outline"></i><i class="mdi mdi-star-outline"></i><i
-                        class="mdi mdi-star-outline"></i>
+        <h3 v-if="account?.rating == 0.5"><i class="mdi mdi-star-half-full star"></i><i class="mdi mdi-star-outline star"></i><i
+                        class="mdi mdi-star-outline star"></i><i class="mdi mdi-star-outline star"></i><i
+                        class="mdi mdi-star-outline star"></i>
         </h3>
-        <h3 v-if="account?.rating == 1"><i class="mdi mdi-star star"></i><i class="mdi mdi-star-outline"></i><i
-                        class="mdi mdi-star-outline"></i><i class="mdi mdi-star-outline"></i><i
-                        class="mdi mdi-star-outline"></i>
+        <h3 v-if="account?.rating == 1"><i class="mdi mdi-star star"></i><i class="mdi mdi-star-outline star"></i><i
+                        class="mdi mdi-star-outline star"></i><i class="mdi mdi-star-outline star"></i><i
+                        class="mdi mdi-star-outline star"></i>
         </h3>
-        <h3 v-if="account?.rating == 1.5"><i class="mdi mdi-star star"></i><i class="mdi mdi-star-half star"></i><i
-                        class="mdi mdi-star-outline"></i><i class="mdi mdi-star-outline"></i><i
-                        class="mdi mdi-star-outline"></i>
+        <h3 v-if="account?.rating == 1.5"><i class="mdi mdi-star star"></i><i class="mdi mdi-star-half-full star"></i><i
+                        class="mdi mdi-star-outline star"></i><i class="mdi mdi-star-outline star"></i><i
+                        class="mdi mdi-star-outline star"></i>
         </h3>
         <h3 v-if="account?.rating == 2"><i class="mdi mdi-star star"></i><i class="mdi mdi-star star"></i><i
-                        class="mdi mdi-star-outline"></i><i class="mdi mdi-star-outline"></i><i
-                        class="mdi mdi-star-outline"></i>
+                        class="mdi mdi-star-outline star"></i><i class="mdi mdi-star-outline star"></i><i
+                        class="mdi mdi-star-outline star"></i>
         </h3>
         <h3 v-if="account?.rating == 2.5"><i class="mdi mdi-star star"></i><i class="mdi mdi-star star"></i><i
-                        class="mdi mdi-star-half star"></i><i class="mdi mdi-star-outline"></i><i
-                        class="mdi mdi-star-outline"></i>
+                        class="mdi mdi-star-half-full star"></i><i class="mdi mdi-star-outline star"></i><i
+                        class="mdi mdi-star-outline star"></i>
         </h3>
         <h3 v-if="account?.rating == 3"><i class="mdi mdi-star star"></i><i class="mdi mdi-star star"></i><i
-                        class="mdi mdi-star star"></i><i class="mdi mdi-star-outline"></i><i class="mdi mdi-star-outline"></i>
+                        class="mdi mdi-star star"></i><i class="mdi mdi-star-outline star"></i><i
+                        class="mdi mdi-star-outline star"></i>
         </h3>
         <h3 v-if="account?.rating == 3.5"><i class="mdi mdi-star star"></i><i class="mdi mdi-star star"></i><i
-                        class="mdi mdi-star star"></i><i class="mdi mdi-star-half  star"></i><i
-                        class="mdi mdi-star-outline"></i>
+                        class="mdi mdi-star star"></i><i class="mdi mdi-star-half-full  star"></i><i
+                        class="mdi mdi-star-outline star"></i>
         </h3>
         <h3 v-if="account?.rating == 4"><i class="mdi mdi-star star"></i><i class="mdi mdi-star star"></i><i
-                        class="mdi mdi-star star"></i><i class="mdi mdi-star star"></i><i class="mdi mdi-star-outline"></i></h3>
+                        class="mdi mdi-star star"></i><i class="mdi mdi-star star"></i><i class="mdi mdi-star-outline star"></i>
+        </h3>
         <h3 v-if="account?.rating == 4.5"><i class="mdi mdi-star star"></i><i class="mdi mdi-star star"></i><i
-                        class="mdi mdi-star star"></i><i class="mdi mdi-star star"></i><i class="mdi mdi-star-half star"></i>
+                        class="mdi mdi-star star"></i><i class="mdi mdi-star star"></i><i
+                        class="mdi mdi-star-half-full star"></i>
         </h3>
         <h3 v-if="account?.rating == 5"><i class="mdi mdi-star star"></i><i class="mdi mdi-star star"></i><i
                         class="mdi mdi-star star"></i><i class="mdi mdi-star star"></i><i class="mdi mdi-star star"></i></h3>
