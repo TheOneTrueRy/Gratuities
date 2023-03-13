1c176f8522ddff451f5d6457c7d36d9caebbcb22
Jonah <jonah.osito@icloud.com>
2023-03-13 15:11:30 -0600

    just a v-if

--- Gratuities.client/src/pages/ProfilePage.vue
+++ Gratuities.client/src/pages/ProfilePage.vue
@@ -10,7 +10,7 @@
                                 <ProfileStarRating :rating="profile?.rating" />
                             </div>
                         </div>
-                        <div class="col-6 offset-md-6 text-end">
+                        <div class="col-6 offset-md-6 text-end" v-if="account?.id == !profile?.id">
                             <div class="btn dropstart">
                                 <button class="btn btn-secondary-outline btn-sm dropdown-toggle fs-4" type="button"
                                     data-bs-toggle="dropdown" aria-expanded="false">
@@ -137,6 +137,7 @@ export default {
             profiles: computed(() => AppState.profiles),
             reviews: computed(() => AppState.reviews),
             businesses: computed(() => AppState.businesses),
+            account: computed(() => AppState.account)
         };
     },
     components: { ProfileCard, ReviewCard, ProfileCarousel, RateProfileOffcanvas, TipUserModal, ProfileStarRating, AddEmployee }
