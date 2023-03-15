c0defa7fd406ed9534a0696f282b957a9d0f9375
JoeCalvi <joecalvi@me.com>
2023-03-15 12:38:40 -0600

    about page styling

--- Gratuities.client/src/pages/AboutPage.vue
+++ Gratuities.client/src/pages/AboutPage.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-12 col-md-6 offset-md-3 d-flex justify-content-center mt-1">
+      <div class="col-12 col-md-6 offset-md-3 d-flex justify-content-center mt-2">
         <h1 class="w-ease"><i>Tipping With Ease.</i></h1>
       </div>
       <div
-        class="offset-1 col-10 col-md-8 offset-md-2 mb-2 mt-2 desc-card rounded d-flex justify-content-center elevation-5">
-        <p>&nbsp; &nbsp; &nbsp;The Gratuities App is all about giving deserving workers their due.
+        class="offset-1 col-10 col-md-8 offset-md-2 mb-2 mt-1 desc-card rounded d-flex justify-content-center">
+        <p class="p-2 pt-3">&nbsp; &nbsp; &nbsp;The Gratuities App is all about giving deserving workers their due.
           <br><br>
           &nbsp; &nbsp; &nbsp;
           Ever run into the problem of doing your best but still having to share your tips with co-workers not pulling
@@ -20,7 +20,9 @@
           more towards a mobile application.
         </p>
       </div>
-      <div class="col-12 col-md-6 offset-md-3 mt-3">
+
+      <!-- SECTION dev carousel -->
+      <div class="col-12 col-md-6 offset-md-3 mt-3 mb-2">
         <div id="carouselExampleCaptions" class="carousel slide">
           <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
@@ -38,11 +40,14 @@
 
             <!-- SECTION Joe's card -->
             <div class="carousel-item active">
-              <p class="bio rounded bg-gradient">After a decade of customer-facing roles, Joe was tired...a lover of both
-                art and logic, coding and software development seemed to be the path forward. His end goal is to work in
-                game development but wants all the experience he can get until then. 5th
-                most handsome member of the Gratuities teamn.</p>
-              <img src="https://ca.slack-edge.com/T1GGQN1RU-U04JUTAK8CE-7fb60e9eb27e-512" class="d-block w-100" alt="...">
+              <div class="d-flex justify-content-center">
+                <p class="bio rounded bg-gradient px-3 d-flex align-items-center">After a decade of customer-facing roles,
+                  Joe was tired...a lover of both
+                  art and logic, coding and software development seemed to be the path forward. His end goal is to work in
+                  game development but wants all the experience he can get until then. 5th
+                  most handsome member of the Gratuities team.</p>
+              </div>
+              <img src="https://ca.slack-edge.com/T1GGQN1RU-U04JUTAK8CE-7fb60e9eb27e-512" class="d-block w-100 rounded" alt="...">
               <div class="carousel-caption d-flex align-items-center flex-column">
                 <h5 class="our-names">Joe Calvi</h5>
                 <p class="gratuities-links d-flex justify-content-around rounded-pill px-3"><a target="_blank"
@@ -50,13 +55,17 @@
                     href="https://www.linkedin.com/in/joe-calvi/" class="mdi mdi-linkedin text-light"></a></p>
               </div>
             </div>
+
             <!-- SECTION Ry's Card -->
             <div class="carousel-item">
-              <p class="bio rounded bg-gradient">Video Game GOD and Official "Funniest Man Alive" randomly gets into
-                coding due to actually needing money (we live in a society). 5th most handsome developer on the
-                Gratuities
-                team.</p>
-              <img src="https://ca.slack-edge.com/T1GGQN1RU-U04KR6PJ7FS-06b92423e603-512" class="d-block w-100" alt="...">
+              <div class="d-flex justify-content-center">
+                <p class="bio rounded bg-gradient px-3 d-flex align-items-center">Video Game GOD and Official "Funniest Man
+                  Alive" randomly gets into
+                  coding due to actually needing money (we live in a society). 5th most handsome developer on the
+                  Gratuities
+                  team.</p>
+              </div>
+              <img src="https://ca.slack-edge.com/T1GGQN1RU-U04KR6PJ7FS-06b92423e603-512" class="d-block w-100 rounded" alt="...">
               <div class="carousel-caption d-flex align-items-center flex-column">
                 <h5 class="our-names">Ryan "Ry" Wittman</h5>
                 <p class="gratuities-links d-flex justify-content-around rounded-pill px-3"><a target="_blank"
@@ -64,12 +73,16 @@
                     href="https://www.linkedin.com/in/theonetruery/" class="mdi mdi-linkedin text-light"></a></p>
               </div>
             </div>
+
             <!-- SECTION Andrew's Card -->
             <div class="carousel-item">
-              <p class="bio rounded bg-gradient">Went into Coding in College. Sought more education at CodeWorks. Fifth
-                most handsome man on the Gratuities dev team. Love being with friends and making new coding projects.
-                Boardgame enthusiast, driven to learn, and dedicated to coding. </p>
-              <img src="https://ca.slack-edge.com/T1GGQN1RU-U04JYH9MQ3X-085de4f9b941-512" class="d-block w-100" alt="...">
+              <div class="d-flex justify-content-center">
+                <p class="bio rounded bg-gradient px-3 d-flex align-items-center">Went into Coding in College. Sought more
+                  education at CodeWorks. Fifth
+                  most handsome man on the Gratuities dev team. Love being with friends and making new coding projects.
+                  Boardgame enthusiast, driven to learn, and dedicated to coding. </p>
+              </div>
+              <img src="https://ca.slack-edge.com/T1GGQN1RU-U04JYH9MQ3X-085de4f9b941-512" class="d-block w-100 rounded" alt="...">
               <div class="carousel-caption d-flex align-items-center flex-column">
                 <h5 class="our-names">Andrew Meyers</h5>
                 <p class="gratuities-links d-flex justify-content-around rounded-pill px-3"><a target="_blank"
@@ -78,15 +91,19 @@
                 </p>
               </div>
             </div>
+
             <!-- SECTION Bryson's Card -->
             <div class="carousel-item">
-              <p class="bio rounded bg-gradient">Playing and modding video games my whole life has led me to a deep
-                interest in how it
-                all actually works. Since then I've learned that programming comes quite easy to me. Thanks to CodeWorks
-                I've been able to expand my knowledge in programming and web development. 1st, 2nd, and 3rd most handsome
-                developer in
-                Gratuities dev team.</p>
-              <img src="https://ca.slack-edge.com/T1GGQN1RU-U04JLV5G4NB-7e93e64f0292-512" class="d-block w-100" alt="...">
+              <div class="d-flex justify-content-center">
+                <p class="bio rounded bg-gradient px-3 d-flex align-items-center">Playing and modding video games my whole
+                  life has led me to a deep
+                  interest in how it
+                  all actually works. Thanks to CodeWorks
+                  I've been able to expand my knowledge in programming and web development. 1st, 2nd, and 3rd most handsome
+                  developer in
+                  Gratuities dev team.</p>
+              </div>
+              <img src="https://ca.slack-edge.com/T1GGQN1RU-U04JLV5G4NB-7e93e64f0292-512" class="d-block w-100 rounded" alt="...">
               <div class="carousel-caption d-flex align-items-center flex-column">
                 <h5 class="our-names">Bryson Bloom</h5>
                 <p class="gratuities-links d-flex justify-content-around rounded-pill px-3"><a target="_blank"
@@ -95,13 +112,17 @@
                 </p>
               </div>
             </div>
+
             <!-- SECTION Jonah's Card -->
             <div class="carousel-item">
-              <p class="bio">Being an avid LEGO builder since I was quite young, I've always had a love for the art of
-                creation and
-                programming gives exactly that. 5th most handsome developer on the Gratuities team.
-              </p>
-              <img src="https://ca.slack-edge.com/T1GGQN1RU-U04K3VA3544-b04aee2f5a06-512" class="d-block w-100" alt="...">
+              <div class="d-flex justify-content-center">
+                <p class="bio rounded bg-gradient px-3 d-flex align-items-center">Being an avid LEGO builder since I was
+                  quite young, I've always had a love for the art of
+                  creation and
+                  programming gives exactly that. 5th most handsome developer on the Gratuities team.
+                </p>
+              </div>
+              <img src="https://ca.slack-edge.com/T1GGQN1RU-U04K3VA3544-b04aee2f5a06-512" class="d-block w-100 rounded" alt="...">
               <div class="carousel-caption d-flex align-items-center flex-column">
                 <h5 class="our-names">Jonah Wood</h5>
                 <p class="gratuities-links d-flex justify-content-around rounded-pill px-3"><a target="_blank"
@@ -110,6 +131,7 @@
               </div>
             </div>
           </div>
+
           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
             data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -120,17 +142,15 @@
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
           </button>
+
         </div>
       </div>
+
     </div>
   </div>
 </template>
 
 <script>
-import { watchEffect } from 'vue';
-import { accountService } from '../services/AccountService';
-import { notificationsService } from '../services/NotificationsService';
-import { tipsService } from '../services/TipsService';
 
 export default {
   setup() {
@@ -146,8 +166,10 @@ export default {
   background-color: #06d69e5d;
   font-family: 'Lato', sans-serif;
   font-size: 2vh;
-  height: 12vh;
+  height: 20vh;
+  width: 95%;
   overflow-y: scroll;
+  box-shadow: 5px 5px 2px 1px #0158415d;
 }
 
 .bio::-webkit-scrollbar {
@@ -163,6 +185,8 @@ export default {
   font-weight: 200;
   background-color: #06D6A0;
   color: white;
+  text-shadow: 1px 1px 2px black;
+  box-shadow: 5px 5px 2px 1px #0158415d;
 }
 
 img {
