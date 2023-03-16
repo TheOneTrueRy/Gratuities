51e7e10bfc7134d94bd30ce6cc9a6c6e696f45d9
JoeCalvi <joecalvi@me.com>
2023-03-16 09:49:46 -0600

    user doesn't see tip form if currency = 0

--- Gratuities.client/src/components/TipUserModal.vue
+++ Gratuities.client/src/components/TipUserModal.vue
@@ -8,15 +8,22 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <form @submit.prevent="sendTip()">
+                        <form v-if="account?.currency > 0" @submit.prevent="sendTip()">
                             <div class="input-group mb-3">
-                                <input v-model="editable.tip" type="number" min="0.01" :max="account.currency" step=".01" class="form-control"
+                                <input v-model="editable.tip" type="number" min="0.01" :max="account?.currency" step=".01" class="form-control"
                                     placeholder="tip amount" aria-label="Recipient's username"
                                     aria-describedby="button-addon2">
                                 <button class="btn btn-outline-light modal-button" type="submit" required
                                     data-bs-dismiss="modal" id="button-addon2">Send</button>
                             </div>
                         </form>
+                        <div v-if="account?.currency == 0" class="row justify-content-center">
+                            <div class="col-10 text-center">
+                                <h3>
+                                    You must add money to your account in order to send a tip.
+                                </h3>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -47,9 +54,6 @@ export default {
                     const tip = editable.value
                     const profileId = route.params.profileId
                     await tipsService.sendTip(profileId, tip)
-                    // if(account.currency) {
-
-                    // }
                     editable.value = {}
                 } catch (error) {
                     Pop.error('[SENDING TIP]', error)
