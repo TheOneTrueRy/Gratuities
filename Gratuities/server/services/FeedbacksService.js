52aaaa4b0cf20afb9c9cb79e0784c4f642c21e6a
Andrew Meyers <laxmeyers@gmail.com>
2023-03-17 10:20:09 -0600

    checking to mkae sure you dont talk to yourself

--- Gratuities/server/services/FeedbacksService.js
+++ Gratuities/server/services/FeedbacksService.js
@@ -1,7 +1,11 @@
 import { dbContext } from "../db/DbContext"
+import { Forbidden } from "../utils/Errors"
 
 class FeedbacksService {
     async sendFeedback(body) {
+        if (body.receiverId == body.giverId) {
+            throw new Forbidden("You can't give yourself feedback")
+        }
         const feedback = await dbContext.Feedbacks.create(body)
         await feedback.populate('giver reciever', 'name picture')
         return feedback
