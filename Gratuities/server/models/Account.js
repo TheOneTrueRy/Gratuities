59557093f635adc89c13bfd693cbdc627c033a2b
Ryan Wittman <OfficiallyRy@outlook.com>
2023-03-21 16:33:18 -0600

    PUT MAXES ON THE BACKEND STRINGS

--- Gratuities/server/models/Account.js
+++ Gratuities/server/models/Account.js
@@ -5,11 +5,11 @@ export const AccountSchema = new Schema(
   {
     subs: [{ type: String, unique: true }],
     email: { type: String, lowercase: true, unique: true },
-    name: { type: String, required: true },
+    name: { type: String, required: true, max: 15 },
     picture: { type: String },
     // NOTE If you wish to add additional properties do so here
     currency: { type: Number, required: true, default: 0.00, min: 0 },
-    bio: { type: String, default: '' },
+    bio: { type: String, default: '', max: 200 },
     openToFeedback: { type: Boolean, default: false },
     tips: { type: Number, default: 0.00, min: 0 },
     rating: { type: Number, default: 0, min: 0, max: 5 },
