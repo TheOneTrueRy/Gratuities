cc123716057ceeda3db948ea8d2669f94ea3a7e2
Ry <OfficiallyRy@outlook.com>
2023-03-20 22:29:14 -0600

    no that didnt work go back go back

--- Gratuities.client/src/env.js
+++ Gratuities.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : 'https://gratuities.onrender.com'
+export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = true
 export const domain = 'theonetruery.us.auth0.com'
 export const clientId = 'akV13tVJ4Z0r96mr8BwmKaFGah40DVxm'
