bfe5050f4f22ec17187ed4b6c420509858db326b
Ryan Wittman <OfficiallyRy@outlook.com>
2023-04-10 09:16:30 -0600

    possible qr fix

--- Gratuities.client/src/services/ProfilesService.js
+++ Gratuities.client/src/services/ProfilesService.js
@@ -39,7 +39,7 @@ class ProfilesService {
     }
 
     async generateQRCode(profileId) {
-        AppState.QRCode = `https://api.qrserver.com/v1/create-qr-code/?data=https://gratuities.onrender.com/#/profiles/${profileId};size=100x100`
+        AppState.QRCode = `https://api.qrserver.com/v1/create-qr-code/?data=https://gratuities.onrender.com/%23/profiles/${profileId};size=100x100`
     }
 
     async leaveReview(reviewData, profileId) {
